ead5406231eac25755c615ee400080a55ce1ea8c
nooneknows peter <jerelepeter06@gmail.com>
2024-12-28 21:33:24 +0000

    feat: added not found function

--- element.sh
+++ element.sh
@@ -5,10 +5,15 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 # global vars
 QUERY_ELEMENT="something"
 
+NOT_FOUND() {
+
+  echo -e "\nI could not find that element in the database."
+}
+
 # empty arg
 if [[ -z $1 ]]; then
 
-  echo "Please provide an element as an argument."
+  echo -e "\nPlease provide an element as an argument."
 
 # when arg is a number
 elif [[ $1 =~ ^[0-9]+$ ]]; then
@@ -16,11 +21,19 @@ elif [[ $1 =~ ^[0-9]+$ ]]; then
   # query database using argument
   QUERY_ELEMENT=$($PSQL "SELECT * FROM elements INNER JOIN properties USING (atomic_number) WHERE atomic_number = $1")
 
-  # present information
-  echo "$QUERY_ELEMENT" | while IFS='|' read ATOMIC_NUMBER ATOMIC_SYMBOL ELEMENT_NAME ELEMENT_TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE_ID; do
+  if [[ -z $QUERY_ELEMENT ]]; then
 
-    echo -e "\nThe element with atomic number $ATOMIC_NUMBER is $ELEMENT_NAME ($ATOMIC_SYMBOL). It's a $ELEMENT_TYPE, with a mass of $ATOMIC_MASS amu. $ELEMENT_NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
-  done
+    NOT_FOUND
+
+  else
+
+    echo "$QUERY_ELEMENT" | while IFS='|' read ATOMIC_NUMBER ATOMIC_SYMBOL ELEMENT_NAME ELEMENT_TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE_ID; do
+
+      echo -e "\nThe element with atomic number $ATOMIC_NUMBER is $ELEMENT_NAME ($ATOMIC_SYMBOL). It's a $ELEMENT_TYPE, with a mass of $ATOMIC_MASS amu. $ELEMENT_NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+    done
+
+  fi
+  # present information
 
 # when arg is a char
 elif [[ $1 = [[:alpha:]] ]]; then
@@ -31,9 +44,9 @@ elif [[ $1 = [[:alpha:]] ]]; then
 elif [[ ! $1 =~ ^[0-9]+$ ]]; then
 
   echo "string"
-  
+
 else
 
-  echo -e "\nI could not find that element in the database."
+  NOT_FOUND
 
 fi
