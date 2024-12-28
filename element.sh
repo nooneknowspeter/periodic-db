3a49728792c68b4156216ac56d1553ac1a0a57cd
nooneknows peter <jerelepeter06@gmail.com>
2024-12-28 21:17:46 +0000

    feat: added more options in conditiional block

--- element.sh
+++ element.sh
@@ -2,8 +2,15 @@
 
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
+# global vars
+QUERY_ELEMENT="something"
+
+# empty arg
 if [[ -z $1 ]]; then
+
   echo "Please provide an element as an argument."
+
+# when arg is a number
 elif [[ $1 =~ ^[0-9]+$ ]]; then
 
   # query database using argument
@@ -13,11 +20,20 @@ elif [[ $1 =~ ^[0-9]+$ ]]; then
   echo "$QUERY_ELEMENT" | while IFS='|' read ATOMIC_NUMBER ATOMIC_SYMBOL ELEMENT_NAME ELEMENT_TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT TYPE_ID; do
 
     echo -e "\nThe element with atomic number $ATOMIC_NUMBER is $ELEMENT_NAME ($ATOMIC_SYMBOL). It's a $ELEMENT_TYPE, with a mass of $ATOMIC_MASS amu. $ELEMENT_NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
-
   done
 
-fi
+# when arg is a char
+elif [[ $1 = [[:alpha:]] ]]; then
 
-PRESENT_INFORMATION() {
+  echo "single char"
+
+# when arg is a string
+elif [[ $1 ]]; then
+
+  echo "string"
   
-}
+else
+
+  echo -e "\nI could not find that element in the database."
+  
+fi
